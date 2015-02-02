08361429bd4057b02e43cff41bac1563534a9f8d
FastFrench <antispam@laposte.net>
2015-02-02 16:54:15 +0100

    Fix BS size detection

--- CsharpCocBot/CsharpCocBot/Functions/Main.cs
+++ CsharpCocBot/CsharpCocBot/Functions/Main.cs
@@ -1,153 +1,153 @@
 ﻿namespace CoC.Bot.Functions
 {
-  using System;
-  using System.Collections.Generic;
-  using System.IO;
-  using System.Linq;
-  using System.Reflection;
-  using System.Text;
-  using System.Threading;
-  using System.Threading.Tasks;
-  using System.Windows;
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Reflection;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using System.Windows;
 
-  using Tools;
-  using Tools.FastFind;
-  using ViewModels;
+	using Tools;
+	using Tools.FastFind;
+	using ViewModels;
 
-  /// <summary>
-  /// The Main entry point for the Bot Functions.
-  /// </summary>
-  public class Main
-  {
-    /// <summary>
-    /// Initializes the Bot.
-    /// </summary>
-    public static void Initialize(MainViewModel vm)
-    {
-      // Store in properties so we can access in the SubFunctions
-      Bot = vm;
+	/// <summary>
+	/// The Main entry point for the Bot Functions.
+	/// </summary>
+	public class Main
+	{
+		/// <summary>
+		/// Initializes the Bot.
+		/// </summary>
+		public static void Initialize(MainViewModel vm)
+		{
+			// Store in properties so we can access in the SubFunctions
+			Bot = vm;
 
-      Bot.Output = string.Format(Properties.Resources.OutputWelcomeMessage, Properties.Resources.AppName);
-      Bot.Output = Properties.Resources.OutputBotIsStarting;
+			Bot.Output = string.Format(Properties.Resources.OutputWelcomeMessage, Properties.Resources.AppName);
+			Bot.Output = Properties.Resources.OutputBotIsStarting;
 
-      // Check if BlueStack is running
-      FastFindWrapper.SetHWnd(BlueStackHelper.GetBlueStackWindowHandle(), true);
-      if (!BlueStackHelper.IsBlueStackRunning)
-      {
-        Bot.Output = Properties.Resources.OutputBSNotFound;
+			// Check if BlueStack is running
+			FastFindWrapper.SetHWnd(BlueStackHelper.GetBlueStackWindowHandle(), true);
+			if (!BlueStackHelper.IsBlueStackRunning)
+			{
+				Bot.Output = Properties.Resources.OutputBSNotFound;
 
-        Bot.IsExecuting = false;
-        return;
-      }
+				Bot.IsExecuting = false;
+				return;
+			}
 
-      if (!BlueStackHelper.IsRunningWithRequiredDimensions)
-      {
-        Bot.Output = Properties.Resources.OutputBSNotRunningWithDimensions;
-        Bot.Output = Properties.Resources.OutputBSApplyDimensionsIntoRegistry;
+			if (!BlueStackHelper.IsRunningWithRequiredDimensions)
+			{
+				Bot.Output = Properties.Resources.OutputBSNotRunningWithDimensions;
+				Bot.Output = Properties.Resources.OutputBSApplyDimensionsIntoRegistry;
 
-        if (!BlueStackHelper.SetDimensionsIntoRegistry())
-        {
-          // Woops! Something went wrong, log the error!
-          Bot.Output = Properties.Resources.OutputBSApplyDimensionsError;
+				if (!BlueStackHelper.SetDimensionsIntoRegistry())
+				{
+					// Woops! Something went wrong, log the error!
+					Bot.Output = Properties.Resources.OutputBSApplyDimensionsError;
 
-          Bot.IsExecuting = false;
-          return;
-        }
-        else
-          Bot.Output = Properties.Resources.OutputBSAppliedDimensionsIntoRegistry;
+					Bot.IsExecuting = false;
+					return;
+				}
+				else
+					Bot.Output = Properties.Resources.OutputBSAppliedDimensionsIntoRegistry;
 
-        // Restart BlueStack
-        // Wait until restart and continue...
+				// Restart BlueStack
+				// Wait until restart and continue...
 
-        BlueStackHelper.ActivateBlueStack();
-      }
+				BlueStackHelper.ActivateBlueStack();
+			}
 
-      CreateDirectory(GlobalVariables.LogPath);
-      CreateDirectory(GlobalVariables.ScreenshotZombieAttacked);
-      CreateDirectory(GlobalVariables.ScreenshotZombieSkipped);
+			CreateDirectory(GlobalVariables.LogPath);
+			CreateDirectory(GlobalVariables.ScreenshotZombieAttacked);
+			CreateDirectory(GlobalVariables.ScreenshotZombieSkipped);
 
-      WriteLicense();
+			WriteLicense();
 
-      // Run Everything related to the bot in the background
-      var thread = new Thread(() =>
-      {
-        while (Bot.IsExecuting)
-        {
-          Thread.Sleep(1000);
-          Bot.Output = "Loop test...";
-          Bot.Output = "Minimum Gold is: " + Bot.MinimumGold; // Changing values on the fly works as expected
-          Bot.Output = "Try changing values on the fly.";
+			// Run Everything related to the bot in the background
+			var thread = new Thread(() =>
+			{
+				while (Bot.IsExecuting)
+				{
+					Thread.Sleep(1000);
+					Bot.Output = "Loop test...";
+					Bot.Output = "Minimum Gold is: " + Bot.MinimumGold; // Changing values on the fly works as expected
+					Bot.Output = "Try changing values on the fly.";
 
-          //SubFunctions.MainScreen.CheckMainScreen();
+					//SubFunctions.MainScreen.CheckMainScreen();
 
-          //SubFunctions.MainScreen.ZoomOut();
+					//SubFunctions.MainScreen.ZoomOut();
 
-          //SubFunctions.Village.TrainTroops();
+					//SubFunctions.Village.TrainTroops();
 
-          //SubFunctions.Village.RequestCC();
+					//SubFunctions.Village.RequestCC();
 
-          //SubFunctions.Village.Collect();
+					//SubFunctions.Village.Collect();
 
-          //Idle();
+					//Idle();
 
-          //SubFunctions.Attack.AttackMain();
-        };
-      })
-      {
-        IsBackground = true
-      };
-      thread.Start();
-    }
+					//SubFunctions.Attack.AttackMain();
+				};
+			})
+			{
+				IsBackground = true
+			};
+			thread.Start();
+		}
 
-    #region Properties
+		#region Properties
 
-    /// <summary>
-    /// Gets the MainViewModel.
-    /// </summary>
-    /// <value>The MainViewModel.</value>
-    internal static MainViewModel Bot { get; private set; }
+		/// <summary>
+		/// Gets the MainViewModel.
+		/// </summary>
+		/// <value>The MainViewModel.</value>
+		internal static MainViewModel Bot { get; private set; }
 
-    #endregion
+		#endregion
 
-    #region Private Methods
+		#region Private Methods
 
-    /// <summary>
-    /// Creates the required directories.
-    /// </summary>
-    /// <param name="path">The path.</param>
-    private static void CreateDirectory(string path)
-    {
-      if (!Directory.Exists(path))
-      {
-        try
-        {
-          Directory.CreateDirectory(path);
-        }
-        catch (Exception)
-        {
-          throw;
-        }
-      }
-    }
+		/// <summary>
+		/// Creates the required directories.
+		/// </summary>
+		/// <param name="path">The path.</param>
+		private static void CreateDirectory(string path)
+		{
+			if (!Directory.Exists(path))
+			{
+				try
+				{
+					Directory.CreateDirectory(path);
+				}
+				catch (Exception)
+				{
+					throw;
+				}
+			}
+		}
 
-    /// <summary>
-    /// Writes the License.
-    /// </summary>
-    private static void WriteLicense()
-    {
-      if (!File.Exists(Path.Combine(GlobalVariables.AppPath, "LICENSE")))
-      {
-        try
-        {
-          File.WriteAllText(Path.Combine(GlobalVariables.AppPath, "LICENSE"), Properties.Resources.LICENSE, Encoding.UTF8);
-        }
-        catch (Exception)
-        {
-          throw;
-        }
-      }
-    }
+		/// <summary>
+		/// Writes the License.
+		/// </summary>
+		private static void WriteLicense()
+		{
+			if (!File.Exists(Path.Combine(GlobalVariables.AppPath, "LICENSE")))
+			{
+				try
+				{
+					File.WriteAllText(Path.Combine(GlobalVariables.AppPath, "LICENSE"), Properties.Resources.LICENSE, Encoding.UTF8);
+				}
+				catch (Exception)
+				{
+					throw;
+				}
+			}
+		}
 
-    #endregion
-  }
+		#endregion
+	}
 }
--- CsharpCocBot/CsharpCocBot/Tools/BlueStackHelper.cs
+++ CsharpCocBot/CsharpCocBot/Tools/BlueStackHelper.cs
@@ -8,132 +8,132 @@ using System.Threading.Tasks;
 
 namespace CoC.Bot.Tools
 {
-  public static class BlueStackHelper
-  {
-    private static IntPtr bshandle = IntPtr.Zero;
+	public static class BlueStackHelper
+	{
+		private static IntPtr bshandle = IntPtr.Zero;
 
-    public static bool IsBlueStacksFound { get { return bshandle != IntPtr.Zero; } }
+		public static bool IsBlueStacksFound { get { return bshandle != IntPtr.Zero; } }
 
-    public static IntPtr GetBlueStackWindowHandle(bool force = false)
-    {
-      if (bshandle == IntPtr.Zero || force)
-        bshandle = Win32.FindWindow("WindowsForms10.Window.8.app.0.33c0d9d", "BlueStacks App Player"); // First try
-      if (bshandle == IntPtr.Zero)
-        bshandle = Win32.FindWindow(null, "BlueStacks App Player"); // Maybe the class name has changes
-      if (bshandle == IntPtr.Zero)
-      {
-        Process[] proc = Process.GetProcessesByName("BlueStacks App Player"); // If failed, then try with .NET functions
-        if (proc == null || proc.Count() == 0)
-          return IntPtr.Zero;
-        bshandle = proc[0].MainWindowHandle;
-      }
-      return bshandle;
-    }
+		public static IntPtr GetBlueStackWindowHandle(bool force = false)
+		{
+			if (bshandle == IntPtr.Zero || force)
+				bshandle = Win32.FindWindow("WindowsForms10.Window.8.app.0.33c0d9d", "BlueStacks App Player"); // First try
+			if (bshandle == IntPtr.Zero)
+				bshandle = Win32.FindWindow(null, "BlueStacks App Player"); // Maybe the class name has changes
+			if (bshandle == IntPtr.Zero)
+			{
+				Process[] proc = Process.GetProcessesByName("BlueStacks App Player"); // If failed, then try with .NET functions
+				if (proc == null || proc.Count() == 0)
+					return IntPtr.Zero;
+				bshandle = proc[0].MainWindowHandle;
+			}
+			return bshandle;
+		}
 
-    public static bool SetDimensionsIntoRegistry()
-    {
-      try
-      {
-        bool value = false;
+		public static bool SetDimensionsIntoRegistry()
+		{
+			try
+			{
+				bool value = false;
 
-        var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\BlueStacks\Guests\Android\FrameBuffer\0", true);
-        if (key != null)
-        {
-          Registry.SetValue(key.Name, "WindowWidth", 0x0000035C, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "WindowHeight", 0x000002D0, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "GuestWidth", 0x0000035C, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "GuestHeight", 0x000002D0, RegistryValueKind.DWord);
+				var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\BlueStacks\Guests\Android\FrameBuffer\0", true);
+				if (key != null)
+				{
+					Registry.SetValue(key.Name, "WindowWidth", 0x0000035C, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "WindowHeight", 0x000002D0, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "GuestWidth", 0x0000035C, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "GuestHeight", 0x000002D0, RegistryValueKind.DWord);
 
-          Registry.SetValue(key.Name, "Depth", 0x00000010, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "FullScreen", 0x00000000, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "WindowState", 0x00000001, RegistryValueKind.DWord);
-          Registry.SetValue(key.Name, "HideBootProgress", 0x00000001, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "Depth", 0x00000010, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "FullScreen", 0x00000000, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "WindowState", 0x00000001, RegistryValueKind.DWord);
+					Registry.SetValue(key.Name, "HideBootProgress", 0x00000001, RegistryValueKind.DWord);
 
-          key.Close();
+					key.Close();
 
-          value = true;
-        }
+					value = true;
+				}
 
-        return value;
-      }
-      catch (Exception exception)
-      {
-        Debug.Assert(false, exception.Message);
-        return false;
-      }
-    }
+				return value;
+			}
+			catch (Exception exception)
+			{
+				Debug.Assert(false, exception.Message);
+				return false;
+			}
+		}
 
-    public static bool Click(int x, int y)
-    {
-      return Click(new Win32.Point(x, y));
-    }
+		public static bool Click(int x, int y)
+		{
+			return Click(new Win32.Point(x, y));
+		}
 
-    public static bool Click(Win32.Point point)
-    {
-      if (bshandle == IntPtr.Zero)
-        bshandle = GetBlueStackWindowHandle();
-      if (bshandle == IntPtr.Zero)
-        return false;
-      MouseHelper.ClickOnPoint(bshandle, point);
-      return true;
-    }
+		public static bool Click(Win32.Point point)
+		{
+			if (bshandle == IntPtr.Zero)
+				bshandle = GetBlueStackWindowHandle();
+			if (bshandle == IntPtr.Zero)
+				return false;
+			MouseHelper.ClickOnPoint(bshandle, point);
+			return true;
+		}
 
-    #region Properties
+		#region Properties
 
-    /// <summary>
-    /// Gets a value indicating whether BlueStacks is running.
-    /// </summary>
-    /// <value><c>true</c> if BlueStacks is running; otherwise, <c>false</c>.</value>
-    public static bool IsBlueStackRunning
-    {
-      get
-      {
-        bshandle = IntPtr.Zero;
-        return GetBlueStackWindowHandle() != IntPtr.Zero;
-      }
-    }
+		/// <summary>
+		/// Gets a value indicating whether BlueStacks is running.
+		/// </summary>
+		/// <value><c>true</c> if BlueStacks is running; otherwise, <c>false</c>.</value>
+		public static bool IsBlueStackRunning
+		{
+			get
+			{
+				bshandle = IntPtr.Zero;
+				return GetBlueStackWindowHandle() != IntPtr.Zero;
+			}
+		}
 
-    /// <summary>
-    /// Gets a value indicating whether BlueStacks is running with required dimensions.
-    /// </summary>
-    /// <value><c>true</c> if this BlueStacks is running with required dimensions; otherwise, <c>false</c>.</value>
-    public static bool IsRunningWithRequiredDimensions
-    {
-      get
-      {
-        var rct = new Win32.RECT();
-        Win32.GetWindowRect(bshandle, out rct);
+		/// <summary>
+		/// Gets a value indicating whether BlueStacks is running with required dimensions.
+		/// </summary>
+		/// <value><c>true</c> if this BlueStacks is running with required dimensions; otherwise, <c>false</c>.</value>
+		public static bool IsRunningWithRequiredDimensions
+		{
+			get
+			{
+				var rct = new Win32.RECT();
+				Win32.GetClientRect(bshandle, out rct);
 
-        var width = rct.Right - rct.Left; // in Win32 Rect, right and bottom are considered as excluded from the rect. 
-        var height = rct.Bottom - rct.Top;
+				var width = rct.Right - rct.Left; // in Win32 Rect, right and bottom are considered as excluded from the rect. 
+				var height = rct.Bottom - rct.Top;
 
-        return (width != 860) || (height != 720) ? false : true;
-      }
-    }
+				return (width != 860) || (height != 720) ? false : true;
+			}
+		}
 
-    #endregion
+		#endregion
 
-    /// <summary>
-    /// Activates and displays the window. If the window is 
-    /// minimized or maximized, the system restores it to its original size 
-    /// and position. An application should use this when restoring 
-    /// a minimized window.
-    /// </summary>
-    /// <returns></returns>
-    public static bool RestoreBlueStack()
-    {
-      if (!IsBlueStackRunning) return false;
-      return Win32.ShowWindow(bshandle, Win32.WindowShowStyle.Restore);
-    }
+		/// <summary>
+		/// Activates and displays the window. If the window is 
+		/// minimized or maximized, the system restores it to its original size 
+		/// and position. An application should use this when restoring 
+		/// a minimized window.
+		/// </summary>
+		/// <returns></returns>
+		public static bool RestoreBlueStack()
+		{
+			if (!IsBlueStackRunning) return false;
+			return Win32.ShowWindow(bshandle, Win32.WindowShowStyle.Restore);
+		}
 
-    /// <summary>
-    /// Activates the window and displays it in its current size and position.
-    /// </summary>
-    /// <returns></returns>
-    public static bool ActivateBlueStack()
-    {
-      if (!IsBlueStackRunning) return false;
-      return Win32.ShowWindow(bshandle, Win32.WindowShowStyle.Show);
-    }
-  }
+		/// <summary>
+		/// Activates the window and displays it in its current size and position.
+		/// </summary>
+		/// <returns></returns>
+		public static bool ActivateBlueStack()
+		{
+			if (!IsBlueStackRunning) return false;
+			return Win32.ShowWindow(bshandle, Win32.WindowShowStyle.Show);
+		}
+	}
 }
