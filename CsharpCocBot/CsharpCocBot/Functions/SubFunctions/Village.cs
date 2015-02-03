5ab8a6d690533902ec14ea6712e428c1df408f05
Phid <Phid@users.noreply.github.com>
2015-02-03 13:48:46 -0600

    Fixed issues caused by merge

--- CsharpCocBot/CsharpCocBot/Functions/SubFunctions/Village.cs
+++ CsharpCocBot/CsharpCocBot/Functions/SubFunctions/Village.cs
@@ -25,7 +25,7 @@ namespace CoC.Bot.Functions
                 Thread.Sleep(2000);
             }
 
-            Main.Bot.Output = "Collecting Resources...";
+            Main.Bot.WriteToOutput("Collecting Resources...");
             Thread.Sleep(250);
             Tools.MouseHelper.ClickOnPoint2(GlobalVariables.HWnD, new Point(1, 1), 1);
 
@@ -84,7 +84,7 @@ namespace CoC.Bot.Functions
             
             if (!GlobalVariables.fullArmy)
             {
-                Main.Bot.Output = "~~~ Waiting for full army ~~~";
+                Main.Bot.WriteToOutput("~~~ Waiting for full army ~~~");
                 while (!GlobalVariables.fullArmy)
                 {
                     sw.Start();
@@ -113,7 +113,7 @@ namespace CoC.Bot.Functions
                     sw.Stop();
 
                     double idleTime = (double) sw.ElapsedMilliseconds * 1000;
-                    Main.Bot.Output = "Time Idle: " + Math.Floor(Math.Floor(idleTime / 60) / 60).ToString() + " hours " + Math.Floor(Math.Floor(idleTime / 60) % 60).ToString() + " minutes " + Math.Floor(idleTime % 60).ToString() + " seconds";
+					Main.Bot.WriteToOutput(string.Format("Time Idle: {0} hours {1} minutes {2} seconds", Math.Floor(Math.Floor(idleTime / 60) / 60), Math.Floor(Math.Floor(idleTime / 60) % 60), Math.Floor(idleTime % 60)), GlobalVariables.OutputStates.Warning);
                 }
             }
         }
@@ -128,7 +128,7 @@ namespace CoC.Bot.Functions
                 Thread.Sleep(1000);
             }
 
-            Main.Bot.Output = "Requesting for Clan Castle Troops...";
+            Main.Bot.WriteToOutput("Requesting for Clan Castle Troops...");
             Tools.MouseHelper.ClickOnPoint2(GlobalVariables.HWnD, ccPos, 1);
             Thread.Sleep(1000);
 
@@ -150,13 +150,13 @@ namespace CoC.Bot.Functions
                 }
                 else
                 {
-                    Main.Bot.Output = "Request's already been made...";
+                    Main.Bot.WriteToOutput("Request's already been made...");
                     Tools.MouseHelper.ClickOnPoint2(GlobalVariables.HWnD, new Point(1, 1), 2);
                 }
             }
             else
             {
-                Main.Bot.Output = "Clan Castle not available...";
+                Main.Bot.WriteToOutput("Clan Castle not available...");
             }
         }
 
@@ -177,7 +177,7 @@ namespace CoC.Bot.Functions
                 Thread.Sleep(1000);
             }
 
-            Main.Bot.Output = "Training Troops...";
+            Main.Bot.WriteToOutput("Training Troops...");
 
             for(int i = 0; i < 4; i++)
             {
@@ -192,7 +192,7 @@ namespace CoC.Bot.Functions
                 
                 if(trainPos.IsEmpty)
                 {
-                    Main.Bot.Output = "Barrack " + (i + 1).ToString() + " is not available...";
+					Main.Bot.WriteToOutput(string.Format("Barrack {0} is not available...", i + 1));
                     Thread.Sleep(500);
                 }
                 else
@@ -236,7 +236,7 @@ namespace CoC.Bot.Functions
 
                 if (trainPos.IsEmpty)
                 {
-                    Main.Bot.Output = "Dark Barrack " + (i + 1).ToString() + " is not available...";
+					Main.Bot.WriteToOutput(string.Format("Dark Barrack {0} is not available...", i + 1));
                     Thread.Sleep(500);
                 }
                 else
@@ -263,7 +263,7 @@ namespace CoC.Bot.Functions
                 Tools.MouseHelper.ClickOnPoint2(GlobalVariables.HWnD, new Point(1, 1), 2, 250);
             }
 
-                Main.Bot.Output = "Training Troops Complete...";
+                Main.Bot.WriteToOutput("Training Troops Complete...");
         }
 
         public static bool TrainIt(int troopKind, int count)
@@ -320,7 +320,7 @@ namespace CoC.Bot.Functions
                     return new Point(218, 438); //-----------
                 default:
                     {
-                        Main.Bot.Output = "Don't know how to train the troop " + troopKind + " yet...";
+						Main.Bot.WriteToOutput(string.Format("Don't know how to train the troop {0} yet...", troopKind));
                         return Point.Empty;
                     }
             }
