3ab9887a33aea2d9fc2fb6062cc660440dacb531
FastFrench <antispam@laposte.net>
2015-02-03 19:06:01 +0100

    Implementation of the magic algorithm in ColorOptimizer

--- CsharpCocBot/ColorOptimizer/Moulinette.cs
+++ CsharpCocBot/ColorOptimizer/Moulinette.cs
@@ -26,31 +26,69 @@ namespace ColorOptimizer
 		public string FileName {get; set;}
 		public Dictionary<int, int> ownStats;
 	}
+
 	public class DataPerCategory
 	{
 		public DataPerCategory()
 		{
-			FileNames = new List<string>();
+			GoodSnaps = new List<FileData>();
+			BadSnaps = new List<FileData>();
 			SelectedColorsSoFar = new Dictionary<int,int>();
 		}
 		public string Label { get; set; }
 		public string Comment { get; set; }
-		List<string> FileNames { get; set; }
+		List<FileData> GoodSnaps { get; set; }
+		List<FileData> BadSnaps { get; set; }
 		Dictionary<int, int> SelectedColorsSoFar;
-		private bool ProcessFirstGoodFile(string fileName)
+		private bool ProcessFirstGoodFile(FileData data)
+		{
+			SelectedColorsSoFar = new Dictionary<int, int>(data.ownStats);
+			return SelectedColorsSoFar.Count > 0;
+		}
+		private bool ProcessNextGoodFile(FileData data)
+		{
+			foreach (int color in data.ownStats.Keys)
+				if (SelectedColorsSoFar.ContainsKey(color))
+					SelectedColorsSoFar[color] += data.ownStats[color];
+				else
+					SelectedColorsSoFar.Remove(color);
+			return SelectedColorsSoFar.Count > 0;
+		}
+		private bool ProcessGoodFiles()
+		{
+			foreach (FileData snap in GoodSnaps)
+				if (SelectedColorsSoFar.Count == 0)
+				{
+					if (!ProcessFirstGoodFile(snap)) return false;
+				}
+				else
+					if (!ProcessNextGoodFile(snap)) return false;
+			return SelectedColorsSoFar.Count>0;
+		}
+		private bool ProcessBadFile(FileData data)
+		{
+			foreach (int color in data.ownStats.Keys)
+				if (SelectedColorsSoFar.ContainsKey(color))
+					SelectedColorsSoFar.Remove(color);					
+			return SelectedColorsSoFar.Count > 0;
+		}
+		private bool ProcessBadFiles()
+		{
+			foreach (FileData snap in BadSnaps)
+				if (!ProcessBadFile(snap)) return false;
+			return SelectedColorsSoFar.Count > 0;
+		}
+		private bool SaveResultInFile()
 		{
 			return false;
 		}
-		private bool ProcessNextGoodFile(string fileName)
+		public bool Process()
 		{
-			return false;
-		}
-		public bool ProcessGoodFiles()
-		{
-			return false;
-		}
-		public bool ProcessBadFile(string fileName)
-		{
+			if (ProcessGoodFiles() && ProcessBadFiles())
+			{
+				if (!SaveResultInFile()) return false;
+				return true;
+			}
 			return false;
 		}
 	}
