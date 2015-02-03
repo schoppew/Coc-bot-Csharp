627fc945f46427cfa021f5c000125fa4b9adf692
Phid <Phid@users.noreply.github.com>
2015-02-03 01:27:27 -0600

    Make the App Singleton

--- CsharpCocBot/CsharpCocBot/App.xaml.cs
+++ CsharpCocBot/CsharpCocBot/App.xaml.cs
@@ -1,18 +1,57 @@
 ﻿namespace CoC.Bot
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Windows;
-    using System.Windows.Media;
-    using System.Windows.Threading;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
+	using System.Windows;
+	using System.Windows.Media;
+	using System.Windows.Threading;
+
+	using Microsoft.Shell;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+	public partial class App : Application, ISingleInstanceApp
     {
+		private const string Unique = "CoC-Bot-c977bfea-3c83-4975-930b-d01c7614447a";
+
+		[STAThread]
+		public static void Main()
+		{
+			if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+			{
+				var application = new App();
+				application.InitializeComponent();
+				application.Run();
+
+				// Allow single instance code to perform cleanup operations
+				SingleInstance<App>.Cleanup();
+			}
+		}
+
+		#region ISingleInstanceApp Members
+
+		public bool SignalExternalCommandLineArgs(IList<string> args)
+		{
+			// Handle command line arguments of second instance
+
+			// Show window if hidden (tray)
+			if (!MainWindow.IsVisible)
+				MainWindow.Show();
+
+			// Bring window to foreground
+			if (MainWindow.WindowState == WindowState.Minimized)
+				MainWindow.WindowState = WindowState.Normal;
+
+			MainWindow.Activate();
+
+			return true;
+		}
+
+		#endregion
+
         public App()
         {
             FieldInfo colorCacheField = typeof(SystemColors).GetField("_colorCache", BindingFlags.Static | BindingFlags.NonPublic);
