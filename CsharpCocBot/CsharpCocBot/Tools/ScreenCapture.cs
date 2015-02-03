e6fe0a16db9103c0bf81544fa13cdf1f45ba3b8c
FastFrench <antispam@laposte.net>
2015-02-03 10:55:46 +0100

    Fixed Screeen Capture
    Now works properly.
    On Windows 8.1, background is useless: even without it, captures are OK
    on hidden windows

--- CsharpCocBot/CsharpCocBot/Tools/ScreenCapture.cs
+++ CsharpCocBot/CsharpCocBot/Tools/ScreenCapture.cs
@@ -16,6 +16,7 @@ namespace CoC.Bot.Tools
     {
         public Bitmap BitMap { get; private set; }
         private IntPtr hBitmap = IntPtr.Zero;
+		public Rectangle CurrentArea { get; private set; }
 
         #region IDisposable interface
         public void Dispose()
@@ -43,6 +44,8 @@ namespace CoC.Bot.Tools
 
         public Bitmap SnapShot(Rectangle rect, bool backgroundMode = true)
         {
+			CurrentArea = rect;
+			ClientToWindow(ref rect);
             return SnapShot(rect.Left, rect.Top, rect.Width, rect.Height, backgroundMode);
         }
 
@@ -57,6 +60,7 @@ namespace CoC.Bot.Tools
         /// <returns></returns>
         public Bitmap SnapShot(int left, int top, int width, int height, bool backgroundMode = true)
         {
+			Stopwatch sw = Stopwatch.StartNew();
             FreeCurrentImage();
             IntPtr hWnd = BlueStackHelper.GetBlueStackWindowHandle();
             if (hWnd == IntPtr.Zero)
@@ -84,17 +88,24 @@ namespace CoC.Bot.Tools
             Win32.ReleaseDC(hWnd, hCaptureDC);
             Win32.DeleteObject(hBitmap);
             Debug.Assert(result);
+			sw.Stop();
+			Debug.WriteLine("SnapShot {0}x{1} => {2} (background:{3})", width, height, sw.Elapsed.ToString(), backgroundMode.ToString());
             return BitMap;
         }
 
-        private bool OffsetToBSClientScrrenCoord(ref Rectangle rect)
-        {
-            if (!BlueStackHelper.IsBlueStackRunning) return false;
-            Win32.Point origin = new Win32.Point(0, 0);
-            if (Win32.ClientToScreen(BlueStackHelper.GetBlueStackWindowHandle(), ref origin)) return false;
-            rect.Offset(origin.X, origin.Y);
-            return true;
-        }
+		private bool ClientToWindow(ref Rectangle rect)
+		{
+			Win32.RECT rWindow= new Win32.RECT();
+		  Win32.RECT rClient= new Win32.RECT();
+			Win32.GetWindowRect(BlueStackHelper.GetBlueStackWindowHandle(), out rWindow);
+			Win32.GetClientRect(BlueStackHelper.GetBlueStackWindowHandle(), out rClient);
+			Point topleft = new Point(rClient.Left, rClient.Top);
+			
+			Win32.ClientToScreen(BlueStackHelper.GetBlueStackWindowHandle(), ref topleft);			
+			rect.Offset(topleft.X - rWindow.Left, topleft.Y - rWindow.Top);
+			return true;
+		}
+
 
         private Rectangle GetBSArea()
         {
@@ -130,7 +141,11 @@ namespace CoC.Bot.Tools
         public Bitmap DotNetSnapShot(Rectangle rect)
         {
             FreeCurrentImage();
-            if (!OffsetToBSClientScrrenCoord(ref rect)) return null;
+			//Win32.RECT wrect = new Win32.RECT(rect);
+			Win32.Point topleft = new Win32.Point(rect.Left, rect.Top);
+			Win32.Point bottomright = new Win32.Point(rect.Right, rect.Bottom);
+			if (!Win32.ClientToScreen(BlueStackHelper.GetBlueStackWindowHandle(), ref topleft)) return null;
+			rect = new Rectangle(topleft.ToNative(), new Size(rect.Width, rect.Height));
 
             //Create a new bitmap.
             BitMap = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
