a94fb733339f93fb37a2ec8f074ec8135395db13
Phid <Phid@users.noreply.github.com>
2015-02-05 00:11:24 -0600

    Settings didn't got in the last commit

--- CsharpCocBot/CsharpCocBot/Properties/Settings.Designer.cs
+++ CsharpCocBot/CsharpCocBot/Properties/Settings.Designer.cs
@@ -169,7 +169,7 @@ namespace CoC.Bot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public int SelectedBarrack3 {
             get {
                 return ((int)(this["SelectedBarrack3"]));
@@ -181,7 +181,7 @@ namespace CoC.Bot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public int SelectedBarrack4 {
             get {
                 return ((int)(this["SelectedBarrack4"]));
