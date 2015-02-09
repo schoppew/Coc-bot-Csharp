e2afd6dd5b4b697625e7b94c64c37736d99186f3
FastFrench <antispam@laposte.net>
2015-02-09 09:50:27 +0100

    Starting Unit Tests

--- CsharpCocBot/UnitTests/UnitTest1.cs
+++ CsharpCocBot/UnitTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using CoC.Bot.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
 	[TestClass]
-	public class UnitTest1
+	public class PixelStuffTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void LaunchBlueStacksAndWait()
 		{
+			
+		}
+
+		[TestMethod]
+		public void CheckScreenCapture()
+		{
+			ExtBitmap.ExtBitmap ebBkgrd = new ExtBitmap.ExtBitmap();
+			ebBkgrd.SnapShot();
+			ExtBitmap.ExtBitmap ebNoBkgrd = new ExtBitmap.ExtBitmap();
+			ebNoBkgrd.SnapShot();
+			ExtBitmap.ExtBitmap ebDotNet = new ExtBitmap.ExtBitmap();
+			ebDotNet.SnapShot();
+			FastFind.FastFindHelper.TakeFullScreenCapture(true);
 		}
 	}
 }
