a396e7dfed56dcad6bfad0909771a8592e7adc70
FastFrench <antispam@laposte.net>
2015-02-23 22:34:19 +0100

    GetString fixed
    Don't know if it works, but should behave more or less  like Autoit
    5.5.1.1 do now

--- CsharpCocBot/CsharpCocBot/BotEngine/ScreenReading/ReadText.cs
+++ CsharpCocBot/CsharpCocBot/BotEngine/ScreenReading/ReadText.cs
@@ -6,13 +6,43 @@ namespace CoC.Bot.BotEngine
 	internal class ReadText
 	{
 
-		private static char GetChar(ref int x, int y)
+		private static char? GetChar(ref int x, int y)
 		{
 			foreach (var pair in ScreenData.OCRData)
 				if (pair.Value.ReadChar(ref x, y))
 					return pair.Key;
-			return '|'; // Don't ask why this...   
+			return null; // Don't ask why this...   
 		}
+
+		public static string GetString(int y)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int xTemp = 35;
+
+				if (GetChar(ref xTemp, y) == null)
+				{
+					y += 1;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			int x = 35;
+			string output = "";
+			char? lastChar = null;
+			do
+			{
+				lastChar = GetChar(ref x, y);
+				if (lastChar == null) break;
+				output += lastChar.Value;
+			} while (!output.EndsWith("  "));
+
+			return output;
+		}
+
 
 		#region Not implemented function
 		public int GetDarkElixir(int _x, int _y)
@@ -43,33 +73,6 @@ namespace CoC.Bot.BotEngine
 		public static int GetOther(int _x, int _y, string kind)
 		{
 			throw new NotImplementedException();
-		}
-
-		public static string GetString(int y)
-		{
-		    for (int i = 0; i < 4; i++)
-		    {
-		        int xTemp = 35;
-                
-		        if (GetChar(ref xTemp, y) == char.Parse("|"))
-		        {
-		            y += 1;
-		        }
-		        else
-		        {
-		            break;
-		        }
-		    }
-
-		    int x = 35;
-		    string output = "";
-
-		    do
-		    {
-		        output += GetChar(ref x, y);
-		    } while (output.Substring(output.Length - 3, output.Length - 1).Equals("  "));
-
-		    return output;
 		}
 
 		public int GetTrophy(int _x, int _y)
